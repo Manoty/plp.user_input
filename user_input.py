5196833f2edbe54045eccaafee9c6f8b544911de
manoty <kevinnoty21@gmail.com>
2024-03-06 12:40:21 +0300

    printed personalized message using python

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask for user's name and store it in the variable "name"
+name = input("Enter your name: kevin")
+
+# Ask for user's age and store it in the variable "age"
+age = input("Enter your age: 23")
+
+# Ask for user's location and store it in the variable "location"
+location = input("Enter your location: Rongai")
+
+# Print out personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
